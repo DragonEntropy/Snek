1f1db2e2cba8cf70fa346280f29f57908eb135c4
DragonEntropy <138557462+DragonEntropy@users.noreply.github.com>
2023-07-04 17:10:12 +1000

    Uploading existing project files
    Note: I did not know multithreading when I made this project. The UI frame will freeze with the current implementation.

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Snek
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.speedBar = new System.Windows.Forms.TrackBar();
+            this.complexityBar = new System.Windows.Forms.TrackBar();
+            this.start = new System.Windows.Forms.Button();
+            this.variantMode = new System.Windows.Forms.CheckBox();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sorryNoHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.madeByAlexanderJephthaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.madeWithMicrosoftVisualStudioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.codedInCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.complexityBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(879, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sorryNoHelpToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Complexity";
+            // 
+            // speedBar
+            // 
+            this.speedBar.Location = new System.Drawing.Point(68, 32);
+            this.speedBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.speedBar.Maximum = 5;
+            this.speedBar.Minimum = 1;
+            this.speedBar.Name = "speedBar";
+            this.speedBar.Size = new System.Drawing.Size(200, 56);
+            this.speedBar.TabIndex = 3;
+            this.speedBar.Value = 1;
+            // 
+            // complexityBar
+            // 
+            this.complexityBar.Location = new System.Drawing.Point(372, 31);
+            this.complexityBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.complexityBar.Minimum = 1;
+            this.complexityBar.Name = "complexityBar";
+            this.complexityBar.Size = new System.Drawing.Size(200, 56);
+            this.complexityBar.TabIndex = 4;
+            this.complexityBar.Value = 1;
+            // 
+            // start
+            // 
+            this.start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.start.Location = new System.Drawing.Point(780, 39);
+            this.start.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(85, 23);
+            this.start.TabIndex = 5;
+            this.start.Text = "Start";
+            this.start.UseMnemonic = false;
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // variantMode
+            // 
+            this.variantMode.AutoSize = true;
+            this.variantMode.Location = new System.Drawing.Point(600, 40);
+            this.variantMode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.variantMode.Name = "variantMode";
+            this.variantMode.Size = new System.Drawing.Size(116, 20);
+            this.variantMode.TabIndex = 7;
+            this.variantMode.Text = "Variant mode?";
+            this.variantMode.UseVisualStyleBackColor = true;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.mainPanel.Location = new System.Drawing.Point(13, 68);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(853, 492);
+            this.mainPanel.TabIndex = 8;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.madeByAlexanderJephthaToolStripMenuItem,
+            this.madeWithMicrosoftVisualStudioToolStripMenuItem,
+            this.codedInCToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // sorryNoHelpToolStripMenuItem
+            // 
+            this.sorryNoHelpToolStripMenuItem.Name = "sorryNoHelpToolStripMenuItem";
+            this.sorryNoHelpToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sorryNoHelpToolStripMenuItem.Text = "Sorry, no help :(";
+            // 
+            // madeByAlexanderJephthaToolStripMenuItem
+            // 
+            this.madeByAlexanderJephthaToolStripMenuItem.Name = "madeByAlexanderJephthaToolStripMenuItem";
+            this.madeByAlexanderJephthaToolStripMenuItem.Size = new System.Drawing.Size(319, 26);
+            this.madeByAlexanderJephthaToolStripMenuItem.Text = "Made by Alexander Jephtha";
+            // 
+            // madeWithMicrosoftVisualStudioToolStripMenuItem
+            // 
+            this.madeWithMicrosoftVisualStudioToolStripMenuItem.Name = "madeWithMicrosoftVisualStudioToolStripMenuItem";
+            this.madeWithMicrosoftVisualStudioToolStripMenuItem.Size = new System.Drawing.Size(319, 26);
+            this.madeWithMicrosoftVisualStudioToolStripMenuItem.Text = "Made with Microsoft Visual Studio";
+            // 
+            // codedInCToolStripMenuItem
+            // 
+            this.codedInCToolStripMenuItem.Name = "codedInCToolStripMenuItem";
+            this.codedInCToolStripMenuItem.Size = new System.Drawing.Size(319, 26);
+            this.codedInCToolStripMenuItem.Text = "Coded in C#";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 572);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.variantMode);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.complexityBar);
+            this.Controls.Add(this.speedBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "Snek";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mainForm_KeyDown_1);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.complexityBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar speedBar;
+        private System.Windows.Forms.TrackBar complexityBar;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.CheckBox variantMode;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem madeByAlexanderJephthaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem madeWithMicrosoftVisualStudioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem codedInCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sorryNoHelpToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Snek
+{
+    public partial class mainForm : Form
+    {
+        static Graphics g;
+
+        public static Random rand = new Random();
+        public static int pixelMultiplier = 16;
+        public static (int, int) startPos = (0, 0);
+        public static bool isVariantMode;
+        public static int defaultSpeed = 100;
+
+        public Player player;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            player = new Player();
+            int speed = speedBar.Value; //Taking pre-game user inputs from the form
+            int complexity = complexityBar.Value;
+            bool isVariantMode = variantMode.Checked;
+
+            complexityBar.Enabled = false;
+            speedBar.Enabled = false;
+            variantMode.Enabled = false;
+            start.Enabled = false;
+
+            int panelHeight = mainPanel.Height; //Generating grid size
+            int panelWidth = mainPanel.Width;
+            int gridHeight = panelHeight / pixelMultiplier;
+            int gridWidth = panelWidth / pixelMultiplier;
+
+            bool alive = true;
+            bool[,] binaryField = new bool[gridWidth, gridHeight];
+            Pellet[] pelletList = new Pellet[complexity];
+
+            g = mainPanel.CreateGraphics();
+            mainPanel.Refresh();
+            mainPanel.BackColor = Color.Black;
+
+            await Task.Delay(100);
+            UpdateSquare(startPos.Item1, startPos.Item2, Color.Green, binaryField, true);
+            for (int p = 0; p < complexity; p++)
+            {
+                pelletList[p] = new Pellet(gridWidth, gridHeight, binaryField, isVariantMode);
+            }
+
+            while (alive == true)
+            {
+                await Task.Delay((int)(defaultSpeed /(speed * player.speedMultiplier)));
+                alive = player.Update(binaryField, gridHeight, gridWidth, pelletList, complexity, isVariantMode);
+            }
+            Console.WriteLine("dead :(");
+
+            complexityBar.Enabled = true;
+            speedBar.Enabled = true;
+            variantMode.Enabled = true;
+            start.Enabled = true;
+        }
+
+        public static void UpdateSquare(int xGrid, int yGrid, Color colour, bool[,] binaryField, bool value)
+        {
+            (int, int) cellPos = (xGrid * pixelMultiplier + 1, yGrid * pixelMultiplier + 1);
+            g.FillRectangle(new SolidBrush(colour), cellPos.Item1, cellPos.Item2, pixelMultiplier - 2, pixelMultiplier - 2);
+            binaryField[xGrid, yGrid] = value;
+        }
+
+        private void mainForm_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyValue == (char)Keys.W) || (e.KeyValue == (char)Keys.Up))
+            {
+                Console.WriteLine("Up!");
+                player.newDirection = (0, -1);
+            }
+            else if ((e.KeyValue == (char)Keys.S) || (e.KeyValue == (char)Keys.Down))
+            {
+                Console.WriteLine("Down!");
+                player.newDirection = (0, 1);
+            }
+            else if ((e.KeyValue == (char)Keys.D) || (e.KeyValue == (char)Keys.Right))
+            {
+                Console.WriteLine("Right!");
+                player.newDirection = (1, 0);
+            }
+            else if ((e.KeyValue == (char)Keys.A) || (e.KeyValue == (char)Keys.Left))
+            {
+                Console.WriteLine("Left!");
+                player.newDirection = (-1, 0);
+            }
+        }
+    }
+
+    public class Player
+    {
+        public int xPos = mainForm.startPos.Item1;
+        public int yPos = mainForm.startPos.Item2;
+        //Not used: int score = 0;
+        int snakeLength = 5;
+        int step = 0;
+
+        public float speedMultiplier = 1;
+        int inversion = 1;
+        int trailDecay = 1;
+        int stagger = 1;
+        public int cooldown = 0;
+
+        (int, int) direction = (1, 0);
+        public (int, int) newDirection = (1, 0);
+
+        List<Trail> trailList = new List<Trail> {};
+
+        public Player()
+        {
+            //Places the initial trail tile
+            trailList.Add(new Trail(xPos, yPos));
+        }
+
+        public void Move(int gridHeight, int gridWidth)
+        {
+            //Handles the movement of the player
+            xPos = (xPos + direction.Item1 + gridWidth) % gridWidth;
+            yPos = (yPos + direction.Item2 + gridHeight) % gridHeight;
+            Console.WriteLine(String.Format("The coordinates are ({0}, {1})", xPos, yPos));
+        }
+
+        public void Turn(int inversion)
+        {
+            //Handles the possible turns the player can make
+            if ((direction.Item1 == newDirection.Item2) || (direction.Item2 == newDirection.Item1))
+            {
+                direction.Item1 = inversion * newDirection.Item1;
+                direction.Item2 = inversion * newDirection.Item2;
+            }
+        }
+
+        public void UpdateTrail(bool[,] binaryField)
+        {
+            //Decreases the lifetime on each trail
+            foreach (Trail trail in trailList.ToList())
+            {
+                bool isDead = trail.update(snakeLength, trailDecay);
+                if (isDead)
+                {
+                    trailList.Remove(trail);
+                    mainForm.UpdateSquare(trail.xPos, trail.yPos, Color.Black, binaryField, false);
+                }
+            }
+        }
+
+        public bool Update(bool[,] binaryField, int gridHeight, int gridWidth, Pellet[] pelletList, int complexity, bool isVariantMode)
+        {
+            //The player turns, moves and then the trail is made
+            Turn(inversion);
+            Move(gridHeight, gridWidth);
+            step++;
+
+            //Checks for collisions
+            bool alive = true;
+            if (binaryField[xPos, yPos])
+            {
+                alive = ManageCollision(binaryField, pelletList, complexity, gridWidth, gridHeight, isVariantMode);
+            }
+
+            UpdateTrail(binaryField);
+
+            //Manages cooldowns
+            if (cooldown > 0)
+            {
+                cooldown--;
+                if (cooldown == 0)
+                {
+                    speedMultiplier = 1;
+                    inversion = 1;
+                    trailDecay = 1;
+                    stagger = 1;
+                }
+            }
+
+            //Adds a new trail tile
+            if(step % stagger == 0)
+            {
+                trailList.Add(new Trail(xPos, yPos));
+                mainForm.UpdateSquare(xPos, yPos, Color.Green, binaryField, true);
+            }
+
+            return alive;
+        }
+
+        public bool ManageCollision(bool[,] binaryField, Pellet[] pelletList, int complexity, int gridWidth, int gridHeight, bool isVariantMode)
+        {
+            for (int p = 0; p < complexity; p++)
+            {
+                (int, int) pelletPos = pelletList[p].getPos();
+                if (pelletPos.Item1 == xPos && pelletPos.Item2 == yPos)
+                {
+                    //Applies variants
+                    switch (pelletList[p].variant)
+                    {
+                        case 1:
+                            speedMultiplier = 2;
+                            cooldown = 400;
+                            break;
+                        case 2:
+                            speedMultiplier = 0.5f;
+                            cooldown = 100;
+                            break;
+                        case 3:
+                            inversion = -1;
+                            cooldown = 100;
+                            break;
+                        case 4:
+                            trailDecay = 0;
+                            cooldown = 50;
+                            break;
+                        case 5:
+                            stagger = 2;
+                            cooldown = 100;
+                            break;
+                    }
+
+                    //Cleans pellet from binaryField
+                    mainForm.UpdateSquare(pelletPos.Item1, pelletPos.Item2, Color.Black, binaryField, false);
+
+                    //Creates a new pellet
+                    pelletList[p] = new Pellet(gridWidth, gridHeight, binaryField, isVariantMode);
+                    snakeLength++;
+
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public class Pellet
+    {
+        int xPos;
+        int yPos;
+        public int variant = 0;
+
+        public Pellet(int gridWidth, int gridHeight, bool[,] binaryField, bool isVariantMode)
+        {
+            bool legalPosition = false;
+
+            //Iterates through until a pellet can be placed in a legal position
+            while (legalPosition == false)
+            {
+                xPos = mainForm.rand.Next(gridWidth);
+                yPos = mainForm.rand.Next(gridHeight);
+                if (binaryField[xPos, yPos] == false)
+                {
+                    binaryField[xPos, yPos] = true;
+                    legalPosition = true;
+                }
+            }
+
+            //Possibly assigns a variant under variant mode and its colour
+            Color colour = Color.Red;
+            if (isVariantMode)
+            {
+                variant = mainForm.rand.Next(20);
+                switch (variant)
+                {
+                    case 1:
+                        colour = Color.Yellow;
+                        break;
+                    case 2:
+                        colour = Color.DeepSkyBlue;
+                        break;
+                    case 3:
+                        colour = Color.DeepPink;
+                        break;
+                    case 4:
+                        colour = Color.Lime;
+                        break;
+                    case 5:
+                        colour = Color.MediumPurple;
+                        break;
+                }
+            }
+
+            //Places the pellet in the grid
+            mainForm.UpdateSquare(xPos, yPos, colour, binaryField, true);
+            Console.WriteLine("Pellet at " + xPos + ", " + yPos + " with variant " + variant);
+        }
+
+        public (int, int) getPos()
+        {
+            (int, int) pos = (xPos, yPos);
+            return pos;
+        }
+    }
+
+    public class Trail
+    {
+        public int xPos;
+        public int yPos;
+        int lifetime;
+
+        public Trail(int x, int y)
+        {
+            xPos = x;
+            yPos = y;
+            lifetime = 0;
+        }
+
+        public bool update(int snakeLength, int trailDecay)
+        {
+            lifetime += trailDecay;
+            if (lifetime == snakeLength)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
